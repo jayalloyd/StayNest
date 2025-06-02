83efe4338494fd03a0a63b4da5b332037dd82e96
jayarani.ys <jayaraniys1@gmail.com>
2025-06-02 21:56:28 +0530

    listings schema introduced

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app= express();
 const mongoose=require("mongoose");
-
+const Listing= require("./models/listing.js");
 
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/StayNest');
@@ -10,6 +10,18 @@ main().then(()=>{
     console.log("connected to db");
 }).catch((err)=>{
     console.log(err);
+});
+app.get("/testListing",async(req,res)=>{
+let sampleListing=new Listing({
+    title:"my new villa",
+    description: "by way",
+    price:12000,
+    location:"Kochi",
+    country:"India",
+});
+await sampleListing.save();
+console.log("sample was saved");
+    res.send("successful testing");
 });
 
 
