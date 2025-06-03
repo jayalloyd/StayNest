a450be5c1476e41037d3b7f51a5e564f70b26e09
jayarani.ys <jayaraniys1@gmail.com>
2025-06-03 22:25:37 +0530

    index route implemented

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app= express();
 const mongoose=require("mongoose");
 const Listing= require("./models/listing.js");
-
+const path=require("path");
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));// to parse data
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/StayNest');
 }
@@ -11,20 +15,12 @@ main().then(()=>{
 }).catch((err)=>{
     console.log(err);
 });
-app.get("/testListing",async(req,res)=>{
-let sampleListing=new Listing({
-    title:"my new villa",
-    description: "by way",
-    price:12000,
-    location:"Kochi",
-    country:"India",
-});
-await sampleListing.save();
-console.log("sample was saved");
-    res.send("successful testing");
-});
 
-
+//index route
+app.get("/listings",async(req,res)=>{
+    let allListings=await Listing.find({})
+    res.render("./listings/index.ejs",{allListings});
+});
 app.listen(8080,()=>{
     console.log("server is listening to port 8080");
 });
