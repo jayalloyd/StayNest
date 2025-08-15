4da979474eed186a37f2724e9bb0d8c4e07a9fef
jayarani.ys <jayaraniys1@gmail.com>
2025-08-16 00:26:21 +0530

    using express router -restructured listing

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const ejsmate=require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js")
 const {listingSchema,reviewSchema}= require("./schema.js");
+const listings=require("./routes/listing.js");
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
@@ -30,17 +31,6 @@ main().then(()=>{
     console.log(err);
 });
 
-const validateListing=(req,res,next)=>{
-    let {error}=listingSchema.validate(req.body);//using joi
- 
-   if(error)
-   {
-    let errMsg=error.details.map((el)=> el.message).join(",");
-    throw new ExpressError(400,errMsg);
-   }else{
-    next();
-   }
-};
 
 const validateReview =(req,res,next)=>{
     let {error}=reviewSchema.validate(req.body);//using joi
@@ -53,53 +43,9 @@ const validateReview =(req,res,next)=>{
     next();
    }
 };
-//index route
-app.get("/listings",wrapAsync(async(req,res)=>{
-    let allListings=await Listing.find({})
-    res.render("./listings/index.ejs",{allListings});
-}));
-//new route
-app.get("/listings/new",(req,res)=>{
-res.render("listings/new.ejs");
-});
 
-//create route
-app.post("/listings",validateListing,wrapAsync(async(req,res)=>{
-   
- const newListing=new Listing(req.body.listing);
- 
-   await newListing.save();
-   res.redirect("/listings");
-})
-);
 
-//show route
-app.get("/listings/:id",wrapAsync(async (req,res)=>{
-
-let {id}=req.params;
-const listing=await Listing.findById(id).populate("reviews");
-res.render("listings/show.ejs",{listing});
-}));
-//edit route
-app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
-let {id}=req.params;
-const listing=await Listing.findById(id);
-res.render("listings/edit.ejs",{listing});
-}));
-//update route
-app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
-let {id}=req.params;
-await Listing.findByIdAndUpdate(id,{...req.body.listing});
- res.redirect(`/listings/${id}`);
-}));
-
-//delete route
-app.delete("/listings/:id",wrapAsync(async(req,res)=>{
-let {id}=req.params;
-let deletedListing= await Listing.findByIdAndDelete(id);
-console.log(deletedListing);
-res.redirect("/listings");
-}));
+app.use("/listings",listings);
 
 //review-post route
 app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
@@ -121,7 +67,6 @@ app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
     console.log("review got deleted");
     res.redirect(`/listings/${id}`);
     }));
-
 app.all("/{*all}", (req, res, next) => { 
     next(new ExpressError(404, "Page not found!"));
 });
